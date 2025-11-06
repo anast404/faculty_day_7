104ee50a633f1aa4f8e8db5e702e111a1749b315
Anastasia <>
2025-11-06 11:52:18 +0300

    сalculator

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
@@ -1,18 +1,36 @@
 package ru.tbank.education.school.lesson1
 
-/**
- * Метод для вычисления простых арифметических операций.
- */
-fun calculate(a: Double, b: Double, operation: OperationType): Double? {
-    TODO()
+fun main() {
+    println("5 + 2".calculate())
+    println("10 * 3".calculate())
+    println("8 / 2".calculate())
+    println("5 - 3".calculate())
+    println("5 & 2".calculate())
 }
 
-/**
- * Функция вычисления выражения, представленного строкой
- * @return результат вычисления строки или null, если вычисление невозможно
- * @sample "5 * 2".calculate()
- */
+fun calculate(a: Double, b: Double, operation: OperationType): Double? {
+    return when (operation) {
+        OperationType.ADD -> a + b
+        OperationType.SUBTRACT -> a - b
+        OperationType.MULTIPLY -> a * b
+        OperationType.DIVIDE -> if (b != 0.0) a / b else null
+    }
+}
+
 @Suppress("ReturnCount")
 fun String.calculate(): Double? {
-    TODO()
+    val parts = this.split(" ")
+    if (parts.size != 3) return null
+
+    val a = parts[0].toDoubleOrNull() ?: return null
+    val b = parts[2].toDoubleOrNull() ?: return null
+    val operation = when (parts[1]) {
+        "+" -> OperationType.ADD
+        "-" -> OperationType.SUBTRACT
+        "*" -> OperationType.MULTIPLY
+        "/" -> OperationType.DIVIDE
+        else -> return null
+    }
+
+    return calculate(a, b, operation)
 }
