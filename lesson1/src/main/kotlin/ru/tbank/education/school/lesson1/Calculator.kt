a1cde5cbf936b8e77dd4fa96db86f5f131dfc807
Anastasia <>
2025-11-12 17:34:41 +0300

    calculator + evennumbers

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
@@ -6,9 +6,11 @@ fun main() {
     println("8 / 2".calculate())
     println("5 - 3".calculate())
     println("5 & 2".calculate())
+    println("5 2".calculate())
+    println("5 / 0".calculate())
 }
 
-fun calculate(a: Double, b: Double, operation: OperationType): Double? {
+fun calculate(a: Double, b: Double, operation: OperationType = OperationType.ADD): Double? {
     return when (operation) {
         OperationType.ADD -> a + b
         OperationType.SUBTRACT -> a - b
@@ -20,6 +22,13 @@ fun calculate(a: Double, b: Double, operation: OperationType): Double? {
 @Suppress("ReturnCount")
 fun String.calculate(): Double? {
     val parts = this.split(" ")
+
+    if (parts.size == 2) {
+        val a = parts[0].toDoubleOrNull() ?: return null
+        val b = parts[1].toDoubleOrNull() ?: return null
+        return calculate(a, b)
+    }
+
     if (parts.size != 3) return null
 
     val a = parts[0].toDoubleOrNull() ?: return null
--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/EvenNumbers.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/EvenNumbers.kt
@@ -1,8 +1,22 @@
 package ru.tbank.education.school.lesson1
 
-/**
- * Сумма четных чисел.
- */
 fun sumEvenNumbers(numbers: Array<Int>): Int {
-    TODO()
+    var sum = 0
+    for (number in numbers) {
+        if (number % 2 == 0) {
+            sum += number
+        }
+    }
+    return sum
+}
+
+fun main() {
+    val test1 = arrayOf(1, 2, 3, 4, 5, 6)
+    println(sumEvenNumbers(test1))
+
+    val test2 = arrayOf<Int>()
+    println(sumEvenNumbers(test2))
+
+    val test3 = arrayOf(1, 3, 5, 7, 9)
+    println(sumEvenNumbers(test3))
 }
